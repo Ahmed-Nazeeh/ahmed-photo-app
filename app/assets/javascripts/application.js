39386ab021daf708e9b8a721ae3346ef36e27cd1
Ahmed-Nazeeh <89453140+Ahmed-Nazeeh@users.noreply.github.com>
2022-01-01 13:16:11 +0200

    deploy to heroku - fix terbolinks error

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,10 +4,11 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
-//= require terbolinks
+//= require rails-ujs
 //= require jquery
-//= require jquery_ujs
 //= require twitter/bootstrap
+//= require turbolinks
 //= require_tree .
+
 
 
